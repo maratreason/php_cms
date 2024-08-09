b455b504ebf593c1d0be6a72ea1e8650c9843b08
maratreason <maratreason@mail.ru>
2024-08-09 12:32:39 +0300

    fwat(user) edit IndexController for testing

--- core/user/controller/IndexController.php
+++ core/user/controller/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace core\user\controller;
 
+use core\admin\model\Model;
 use core\base\controller\BaseController;
 
 class IndexController extends BaseController
@@ -14,11 +15,28 @@ class IndexController extends BaseController
 
         $this->init();
 
-        // exit();
-
         $content = $this->render('', compact('name'));
         $header = $this->render(TEMPLATE . 'header');
         $footer = $this->render(TEMPLATE . 'footer');
+
+        $model = Model::instance();
+
+        $res = $model->get('teachers', [
+            'where' => ['id' => '37,38'],
+            'operand' => ['IN'],
+            'join' => [
+                'stud_teachers' => ['on' => ['id', 'teachers']],
+                'students' => [
+                    'fields' => ['name'],
+                    'on' => ['students', 'id']
+                ],
+            ],
+            'join_structure' => true
+        ]);
+
+        print_arr($res);
+
+        exit($res);
 
         return compact('header', 'content', 'footer');
     }
