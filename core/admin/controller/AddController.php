688219a9f166983c4e6c968936ac44009f834cd7
maratreason <maratreason@mail.ru>
2024-08-11 07:30:14 +0300

    refactor(AddController) refactor method  createForeignProperty()

--- core/admin/controller/AddController.php
+++ core/admin/controller/AddController.php
@@ -35,24 +35,10 @@ class AddController extends BaseAdmin
             $this->foreignData[$arr['COLUMN_NAME']][0]['name'] = $rootItems['name'];
         }
 
-        $where = '';
+        $where = [];
         $operand = '';
 
-        $columns = $this->model->showColumns($arr['REFERENCED_TABLE_NAME']);
-
-        $name = '';
-
-        if ($columns['name']) {
-            $name = 'name';
-        } else {
-            foreach ($columns as $key => $value) {
-                if (strpos($key, 'name') !== false) {
-                    $name = $key . ' as name';
-                }
-            }
-
-            if (!$name) $name = $columns['id_row'] . ' as name';
-        }
+        $orderData = $this->createOrderData($arr['REFERENCED_TABLE_NAME']);
 
         if ($this->data) {
             if ($arr['REFERENCED_TABLE_NAME'] === $this->table) {
@@ -63,9 +49,10 @@ class AddController extends BaseAdmin
         }
         
         $foreign = $this->model->get($arr['REFERENCED_TABLE_NAME'], [
-            'fields' => [$arr['REFERENCED_COLUMN_NAME'] . ' as id', $name],
+            'fields' => [$arr['REFERENCED_COLUMN_NAME'] . ' as id', $orderData['name'], $orderData['parent_id']],
             'where' => $where,
-            'operand' => $operand
+            'operand' => $operand,
+            'order' => $orderData['order']
         ]);
 
         if ($foreign) {
