929d0ddbfcff19ec70cc0586459bd9b1909e040c
maratreason <maratreason@mail.ru>
2024-08-13 08:40:06 +0300

    feat(BaseAdmin) create method checkManyToMany()

--- core/admin/controller/BaseAdmin.php
+++ core/admin/controller/BaseAdmin.php
@@ -357,6 +357,9 @@ abstract class BaseAdmin extends BaseController
             $answerSuccess = $this->messages['editSuccess'];
             $answerFail = $this->messages['editFail'];
         }
+
+        $this->checkManyToMany();
+
         // Передаем все объявленные переменные в expansion()
         $this->expansion(get_defined_vars());
 
@@ -705,4 +708,55 @@ abstract class BaseAdmin extends BaseController
 
         }
     }
+
+    protected function checkManyToMany($settings = false)
+    {
+        if (!$settings) $settings = $this->settings ?: Settings::instance();
+
+        $manyToMany = $settings::get('manyToMany');
+
+        if ($manyToMany) {
+            foreach ($manyToMany as $mTable => $tables) {
+                $targetKey = array_search($this->table, $tables);
+
+                if ($targetKey !== false) {
+                    $otherKey = $targetKey ? 0 : 1;
+                    $checkboxlist = $settings::get('templateArr')['checkboxlist'];
+
+                    if (!$checkboxlist || !in_array($tables[$otherKey], $checkboxlist)) continue;
+
+                    $columns = $this->model->showColumns($tables[$otherKey]);
+                    $targetRow = $this->table . '_' . $this->columns['id_row'];
+                    $otherRow = $tables[$otherKey] . '_' . $columns['id_row'];
+
+                    $this->model->delete($mTable, [
+                        'where' => [$targetRow => $_POST[$this->columns['id_row']]]
+                    ]);
+                    // если что-то пришло в POST
+                    if ($_POST[$tables[$otherKey]]) {
+                        $insertArr = [];
+                        $i = 0;
+
+                        foreach ($_POST[$tables[$otherKey]] as $value) {
+                            foreach ($value as $item) {
+                                if ($item) {
+                                    $insertArr[$i][$targetRow] = $_POST[$this->columns['id_row']];
+                                    $insertArr[$i][$otherRow] = $item;
+                                    $i++;
+                                }
+                            }
+                        }
+
+                        if ($insertArr) {
+                            $this->model->add($mTable, [
+                                'fields' => $insertArr
+                            ]);
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+
 }
