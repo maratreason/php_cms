0258bbda758c40b85b98ef4bc15318daf901908e
maratreason <maratreason@mail.ru>
2024-08-14 14:22:39 +0300

    refactor(Admin/Model) create method updateMenuPosition()

--- core/admin/model/Model.php
+++ core/admin/model/Model.php
@@ -12,7 +12,6 @@ class Model extends BaseModel
     public function showForeignKeys($table, $key = false)
     {
         $db = DB_NAME;
-
         $where = '';
 
         if ($key) $where = "AND COLUMN_NAME = '$key' LIMIT 1";
@@ -23,6 +22,105 @@ class Model extends BaseModel
                             CONSTRAINT_NAME <> 'PRIMARY' AND REFERENCED_TABLE_NAME is not null $where";
 
         return $this->query($query);
+    }
 
+    /**
+     * Обновление позиции вывода
+     *
+     * @param string $table - таблица
+     * @param mixed $row - по умолчанию menu_position
+     * @param mixed $where - если id пришел
+     * @param int $end_pos - позиция на которую вставится текущая запись
+     * @param array $update_rows - массив, чтобы мы могли сортировать относительно, например, parent_id
+     * @return void
+     */
+    public function updateMenuPosition($table, $row, $where, $end_pos, $update_rows = [])
+    {
+        if ($update_rows && isset($update_rows['where'])) {
+            $update_rows['operand'] = isset($update_rows['operand']) ? $update_rows['operand'] : ['='];
+
+            if ($where) {
+                $old_data = $this->get($table, [
+                    'fields' => [$update_rows['where'], $row],
+                    'where' => $where
+                ])[0];
+
+                $start_pos = $old_data[$row];
+
+                if ($old_data[$update_rows['where']] !== $_POST[$update_rows['where']]) {
+                    $pos = $this->get($table, [
+                        'fields' => ['COUNT(*) as count'],
+                        'where' => [$update_rows['where'] => $old_data[$update_rows['where']]],
+                        'no_concat' => true
+                    ])[0]['count'];
+
+                    if ($start_pos != $pos) {
+                        $update_where = $this->createWhere([
+                            'where' => [$update_rows['where'] => $old_data[$update_rows['where']]],
+                            'operand' => $update_rows['operand']
+                        ]);
+
+                        $query = "UPDATE $table SET $row = $row - 1 $update_where AND $row <= $pos AND $row > $start_pos";
+
+                        $this->query($query, 'u');
+                    }
+
+                    $start_pos = $this->get($table, [
+                        'fields' => ['COUNT(*) as count'],
+                        'where' => [$update_rows['where'] => $_POST[$update_rows['where']]],
+                        'no_concat' => true
+                    ])[0]['count'] + 1;
+                }
+
+            } else {
+                $start_pos = $this->get($table, [
+                    'fields' => ['COUNT(*) as count'],
+                    'where' => [$update_rows['where'] => $_POST[$update_rows['where']]],
+                    'no_concat' => true
+                ])[0]['count'] + 1;
+
+            }
+
+            if (array_key_exists($update_rows['where'], $_POST)) {
+                $where_equal = $_POST[$update_rows['where']];
+            } elseif (isset($old_data[$update_rows['where']])) {
+                $where_equal = $old_data[$update_rows['where']];
+            } else {
+                $where_equal = NULL;
+            }
+
+            $db_where = $this->createWhere([
+                'where' => [$update_rows['where'] => $where_equal],
+                'operand' => $update_rows['operand']
+            ]);
+
+        } else {
+            if ($where) {
+                // получаем позицию, на которой был элемент
+                $start_pos = $this->get($table, [
+                    'fields' => [$row],
+                    'where' => $where
+                ])[0][$row]; // возвращем число
+
+            } else {
+                $start_pos = $this->get($table, [
+                    'fields' => ['COUNT(*) as count'],
+                    'no_concat' => true,
+                ])[0]['count'] + 1;
+            }
+
+        }
+        // Теперь работаем относительно сортировки
+        $db_where = isset($db_where) ? $db_where . ' AND' : 'WHERE';
+
+        if ($start_pos < $end_pos) {
+            $query = "UPDATE $table SET $row = $row - 1 $db_where $row <= $end_pos AND $row > $start_pos";
+        } elseif ($start_pos > $end_pos) {
+            $query = "UPDATE $table SET $row = $row + 1 $db_where $row >= $end_pos AND $row < $start_pos";
+        } else {
+            return;
+        }
+
+        return $this->query($query, 'u');
     }
 }
