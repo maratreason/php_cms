f53e2f80c0a2d8a7cef82e9fac26a14d7278b708
maratreason <maratreason@mail.ru>
2024-08-14 14:21:16 +0300

    fix(Admin/EditController) create method createData()

--- core/admin/controller/EditController.php
+++ core/admin/controller/EditController.php
@@ -2,38 +2,43 @@
 
 namespace core\admin\controller;
 
+use core\base\exceptions\RouteException;
+
 class EditController extends BaseAdmin
 {
+    protected $action = 'edit';
+
     protected function inputData()
     {
         if (!$this->userId) $this->execBase();
+
+        $this->checkPost();
+        $this->createTableData();
+        $this->createData();
+        $this->createForeignData();
+        $this->createMenuPosition();
+        $this->createRadio();
+        $this->createOutputData();
+        $this->createManyToMany();
+
+        $this->template = ADMIN_TEMPLATE . 'add';
+
+        return $this->expansion();
     }
 
-    protected function checkOldAlias($id)
+    protected function createData()
     {
-        $tables = $this->model->showTables();
+        $id = is_numeric($this->parameters[$this->table])
+            ? $this->clearNum($this->parameters[$this->table])
+            : $this->clearStr($this->parameters[$this->table]);
 
-        if (!empty($tables) && in_array('old_alias', $tables)) {
+        if (!$id) throw new RouteException('Некорректный идентификатор - ' . $id . ' при редактировании таблицы - ' . $this->table);
 
-            $old_alias = $this->model->get($this->table, [
-                'fields' => ['alias'],
-                'where' => [$this->columns['id_row'] => $id]
-            ])[0]['alias'];
+        $this->data = $this->model->get($this->table, [
+            'where' => [$this->columns['id_row'] => $id]
+        ]);
 
-            if (!empty($old_alias) && $old_alias !== $_POST['alias']) {
-
-                $this->model->delete('old_alias', [
-                    'where' => ['alias' => $old_alias, 'table_name' => $this->table]
-                ]);
-
-                $this->model->delete('old_alias', [
-                    'where' => ['alias' => $_POST['alias'], 'table_name' => $this->table]
-                ]);
-
-                $this->model->add('old_alias', [
-                    'fields' => ['alias' => $old_alias, 'table_name' => $this->table, 'table_id' => $id]
-                ]);
-            }
-        }
+        $this->data && $this->data = $this->data[0];
+        
     }
 }
