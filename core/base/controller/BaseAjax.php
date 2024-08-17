0a6970eb2c6fe837df45f961b975762ea8c89967
maratreason <maratreason@mail.ru>
2024-08-17 22:33:02 +0300

    refactor(BaseAjax) remove some code

--- core/base/controller/BaseAjax.php
+++ core/base/controller/BaseAjax.php
@@ -11,23 +11,25 @@ class BaseAjax extends BaseController
         $route = Settings::get('routes');
         $controller = $route['user']['path'] . 'AjaxController';
         $data = $this->isPost() ? $_POST : $_GET;
+        $httpReferer = str_replace('/', '\/', $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['SERVER_NAME'] . PATH . $route['admin']['alias']);
 
-        if (isset($data['ADMIN_MODE'])) {
+        if (isset($data['ADMIN_MODE']) || preg_match('/^' . $httpReferer . '(\/?|$)/', $_SERVER['HTTP_REFERER'])) {
             unset($data['ADMIN_MODE']);
             $controller = $route['admin']['path'] . 'AjaxController';
         }
 
         $controller = str_replace('/', '\\', $controller);
-
         $ajax = new $controller;
-        $ajax->createAjaxData($data);
+        // $ajax->ajax($data);
+
+        $ajax->ajaxData = $data;
+        // $res = $ajax->ajax();
 
         return ($ajax->ajax());
-    }
 
-    protected function createAjaxData($data)
-    {
-        $this->data = $data;
-    }
+        // if (is_array($res) || is_object($res)) $res = json_encode($res);
+        // elseif (is_int($res)) $res = (float) $res;
 
+        // return $res;
+    }
 }
