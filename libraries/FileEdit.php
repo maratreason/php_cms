041f5a93e0b6d63d511aac09ca3eeab382abc6b1
maratreason <maratreason@mail.ru>
2024-08-21 11:08:59 +0300

    chore(libraries/FileEdit)

--- libraries/FileEdit.php
+++ libraries/FileEdit.php
@@ -38,8 +38,8 @@ class FileEdit
 
                         $res_name = $this->createFile($file_arr);
 
-                        // if ($res_name) $this->imgArr[$key][] = $res_name;
-                        if ($res_name) $this->imgArr[$key][$i] = $res_name;
+                        if ($res_name) $this->imgArr[$key][] = $res_name;
+                        // if ($res_name) $this->imgArr[$key][$i] = $res_name;
 
                     }
 
