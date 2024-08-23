776e498ea6f1a4a256d7826e08dd46bd5034e5fe
maratreason <maratreason@mail.ru>
2024-08-23 13:26:30 +0300

    refactor(Admin/BaseAdmin)

--- core/admin/controller/BaseAdmin.php
+++ core/admin/controller/BaseAdmin.php
@@ -403,7 +403,7 @@ abstract class BaseAdmin extends BaseController
     protected function createFiles($id = null)
     {
         $fileEdit = new FileEdit();
-        $this->fileArray = $fileEdit->addFile();
+        $this->fileArray = $fileEdit->addFile($this->table);
 
         if ($id) {
             $this->checkFiles($id);
