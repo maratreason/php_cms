66cfdcf78eda0ee954950ee1c40ec0540e4a83d5
maratreason <maratreason@mail.ru>
2024-08-23 13:26:05 +0300

    feat(Admin/AjaxController) add method search() and edit method ajax() add case search and add case wyswyg_file

--- core/admin/controller/AjaxController.php
+++ core/admin/controller/AjaxController.php
@@ -2,6 +2,8 @@
 
 namespace core\admin\controller;
 
+use libraries\FileEdit;
+
 class AjaxController extends BaseAdmin
 {
     public function ajax()
@@ -24,10 +26,28 @@ class AjaxController extends BaseAdmin
                 case 'change_parent':
                     return $this->changeParent();
                     break;
+                case 'search':
+                    return $this->search();
+                    break;
+                case 'wyswyg_file':
+                    $a = 1;
+                    $fileEdit = new FileEdit();
+                    $fileEdit->setUniqueFile(false);
+                    $file = $fileEdit->addFile($this->clearStr($this->ajaxData['table']) . '/content_file/');
+                    return ['location' => PATH . UPLOAD_DIR . $file[key($file)]];
+                    break;
             }
         }
 
         return json_encode(['success' => '0', 'message' => 'No ajax variable']);
+    }
+
+    protected function search()
+    {
+        $data = $this->clearStr($this->ajaxData['data']);
+        $table = $this->clearStr($this->ajaxData['table']);
+
+        return $this->model->search($data, $table, 20);
     }
 
     protected function changeParent()
