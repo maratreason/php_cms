f5d2d76efee50b1d12c1f9464aa37102bbcc9574
maratreason <maratreason@mail.ru>
2024-08-23 13:25:02 +0300

    feat(libraries/FileEdit) add methods setUniqueFile() and getFiles()

--- libraries/FileEdit.php
+++ libraries/FileEdit.php
@@ -5,31 +5,22 @@ namespace libraries;
 class FileEdit
 {
     protected $imgArr = [];
-
     protected $directory;
+    protected $uniqueFile = true;
 
-    public function addFile($directory = false)
+    public function addFile($directory = '')
     {
-        if (!$directory) {
+        $directory = trim($directory, ' /');
+        $directory .= '/';
 
-            $this->directory = $_SERVER['DOCUMENT_ROOT'] . PATH . UPLOAD_DIR;
-
-        } else {
-
-            $this->directory = $directory;
-
-        }
+        $this->setDirectory($directory);
 
         foreach ($_FILES as $key => $file) {
-            
             if (is_array($file['name'])) {
-                
                 $file_arr = [];
 
                 foreach ($file['name'] as $i => $value) {
-
                     if (!empty($file['name'][$i])) {
-
                         $file_arr['name'] = $file['name'][$i];
                         $file_arr['type'] = $file['type'][$i];
                         $file_arr['tmp_name'] = $file['tmp_name'][$i];
@@ -38,49 +29,36 @@ class FileEdit
 
                         $res_name = $this->createFile($file_arr);
 
-                        if ($res_name) $this->imgArr[$key][$i] = $res_name;
+                        if ($res_name) $this->imgArr[$key][$i] = $directory . $res_name;
                     }
-
                 }
-
             } else {
-
                 if (isset($file['name'])) {
-
                     $res_name = $this->createFile($file);
 
-                    if ($res_name) $this->imgArr[$key] = $res_name;
-
+                    if ($res_name) $this->imgArr[$key] = $directory . $res_name;
                 }
-
             }
-
         }
 
         return $this->getFiles();
-
     }
 
     protected function createFile($file)
     {
         $fileNameArr = explode('.', $file['name']);
-
         $ext = $fileNameArr[count($fileNameArr) - 1];
 
         unset($fileNameArr[count($fileNameArr) - 1]);
 
         $fileName = implode('.', $fileNameArr);
-
         $fileName = (new \libraries\TextModify())->translit($fileName);
-
         $fileName = $this->checkFile($fileName, $ext);
         // Получаем файл с полным путём.
         $fileFullName = $this->directory . $fileName;
         // Перемещение файла.
         if ($this->uploadFile($file['tmp_name'], $fileFullName)) {
-
             return $fileName;
-
         }
 
         return false;
@@ -89,20 +67,29 @@ class FileEdit
     protected function uploadFile($tmpName, $destination)
     {
         if (move_uploaded_file($tmpName, $destination)) return true;
-
         return false;
     }
     // $fileLastName - динамически генерируется название если такой файл уже существует.
     protected function checkFile($fileName, $ext, $fileLastName = '')
     {
         // Если такого файла не существует, то возвращаем готовое имя файла
-        if (!file_exists($this->directory . $fileName . $fileLastName . '.' . $ext)) {
-
+        if (!file_exists($this->directory . $fileName . $fileLastName . '.' . $ext) || !$this->uniqueFile) {
             return $fileName . $fileLastName . '.' . $ext;
-
         }
         // Иначе дополняем название _, хэшом и датой с конкатенацией со случайным числом. Можно вместо hash использовать md5(). Но hash возвращает короткий хэш. Хэш 'crc32' один из самых коротких.
         return $this->checkFile($fileName, $ext, '_' . hash('crc32', time() . mt_rand(1, 1000)));
+    }
+
+    public function setUniqueFile($value)
+    {
+        $this->uniqueFile = $value ? true : false;
+    }
+
+    public function setDirectory($directory)
+    {
+        $this->directory = $_SERVER['DOCUMENT_ROOT'] . PATH . UPLOAD_DIR . $directory;
+
+        if (!file_exists($this->directory)) mkdir($this->directory, 0777, true);
     }
 
     public function getFiles()
