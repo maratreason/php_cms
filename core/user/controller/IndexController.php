88cc6dd8679586478fb7646a6dd978e4ea4813c1
maratreason <maratreason@mail.ru>
2024-08-26 09:25:37 +0300

    refactor(User/Controller) edit IndexController

--- core/user/controller/IndexController.php
+++ core/user/controller/IndexController.php
@@ -11,63 +11,12 @@ class IndexController extends BaseController
 
     protected function inputData()
     {
-        echo '<div>';
-
-        echo 'Hello World';
-
-        $user = ['name' => 'john', 'age' => 30];
-        // if (isset($user['name'])) {
-        //     $name = $user['name'];
-        // } else {
-        //     $name = 'unknown';
-        // }
-
-        $name = $user['name'] ?? 'unknown';
-
-        echo $name;
-
-        echo '</div>';
-
-        $name = 'Ivan';
-
         $this->init();
 
-        $content = $this->render('', compact('name'));
         $header = $this->render(TEMPLATE . 'header');
+        $content = $this->render();
         $footer = $this->render(TEMPLATE . 'footer');
 
-        // $model = Model::instance();
-
-        // $res = $model->get('goods', [
-        //     'where' => ['id' => '37,38'],
-        //     'operand' => ['IN'],
-        //     'join' => [
-        //         'goods_filters' => [
-        //             'fields' => null,
-        //             'on' => ['id', 'teachers']
-        //         ],
-        //         'filters f' => [
-        //             'fields' => ['name as student_name', 'content'],
-        //             'on' => ['students', 'id']
-        //         ],
-        //         'filters' => [
-        //             'on' => ['parent_id', 'id']
-        //         ],
-        //     ],
-        //     'join_structure' => true,
-        //     'order' => ['id'],
-        //     'order_direction' => ['DESC']
-        // ]);
-
-        // exit(print_arr($res));
-
-        return compact('header', 'content', 'footer');
-    }
-
-    protected function outputData()
-    {
-        $vars = func_get_arg(0);
-        
-        $this->page = $this->render(TEMPLATE . 'templater', $vars);
+        return $this->render(TEMPLATE . 'templater', compact('header', 'content', 'footer'));
     }
 }
