e056bf0ee1a4346a6912569ac882e6d4280a7f73
maratreason <maratreason@mail.ru>
2024-08-26 09:24:41 +0300

    refactor(Base/BaseController) add regexp to method init()

--- core/base/controller/BaseController.php
+++ core/base/controller/BaseController.php
@@ -35,25 +35,25 @@ abstract class BaseController
         if (!$admin) {
             if (USER_CSS_JS['styles']) {
                 foreach (USER_CSS_JS['styles'] as $item) {
-                    $this->styles[] = PATH . TEMPLATE . trim($item, '/');
+                    $this->styles[] = (!preg_match('/^\s*https?:\/\//i', $item) ? PATH . TEMPLATE : '') . trim($item, '/');
                 }
             }
 
             if (USER_CSS_JS['scripts']) {
                 foreach (USER_CSS_JS['scripts'] as $item) {
-                    $this->scripts[] = PATH . TEMPLATE . trim($item, '/');
+                    $this->scripts[] = (!preg_match('/^\s*https?:\/\//i', $item) ? PATH . TEMPLATE : '') . trim($item, '/');
                 }
             }
         } else {
             if (ADMIN_CSS_JS['styles']) {
                 foreach (ADMIN_CSS_JS['styles'] as $item) {
-                    $this->styles[] = PATH . ADMIN_TEMPLATE . trim($item, '/');
+                    $this->styles[] = (!preg_match('/^\s*https?:\/\//i', $item) ? PATH . ADMIN_TEMPLATE : '') . trim($item, '/');
                 }
             }
 
             if (ADMIN_CSS_JS['scripts']) {
                 foreach (ADMIN_CSS_JS['scripts'] as $item) {
-                    $this->scripts[] = PATH . ADMIN_TEMPLATE . trim($item, '/');
+                    $this->scripts[] = (!preg_match('/^\s*https?:\/\//i', $item) ? PATH . ADMIN_TEMPLATE : '') . trim($item, '/');
                 }
             }
         }
