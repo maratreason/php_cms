135b89e799f4bb742087b3b7bea8ecddb4801733
maratreason <maratreason@mail.ru>
2024-08-27 15:08:53 +0300

    refactor(internal_settings) edit cookie_time

--- core/base/settings/internal_settings.php
+++ core/base/settings/internal_settings.php
@@ -12,7 +12,7 @@ const DEFAULT_IMAGE_DIRECTORY = 'default_images';
 
 const COOKIE_VERSION = '1.0.0';
 const CRYPT_KEY = 's5v8y/A?D(G+KbPeXn2r5u8x/A%D*G-KfTjWnZr4u7x!A%C*MbQeThWmZq4t7w!z(G+KbPeShVmYq3t6A%D*G-KaPdSgVkYp7x!z%C*F-JaNdRgUq4t7w9z$C&F)J@Nc';
-const COOKIE_TIME = 60;
+const COOKIE_TIME = 120;
 const BLOCK_TIME = 3;
 
 const END_SLASH = '/';
