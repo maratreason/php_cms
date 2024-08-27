d335c9c75c42d8d7cee35c16046d74c1f86d0345
maratreason <maratreason@mail.ru>
2024-08-27 10:41:13 +0300

    feat(internal_settings) add 2 constants

--- core/base/settings/internal_settings.php
+++ core/base/settings/internal_settings.php
@@ -8,12 +8,14 @@ const MS_MODE = false;
 const TEMPLATE = 'templates/default/';
 const ADMIN_TEMPLATE = 'core/admin/view/';
 const UPLOAD_DIR = 'userfiles/';
+const DEFAULT_IMAGE_DIRECTORY = 'default_images';
 
 const COOKIE_VERSION = '1.0.0';
 const CRYPT_KEY = 's5v8y/A?D(G+KbPeXn2r5u8x/A%D*G-KfTjWnZr4u7x!A%C*MbQeThWmZq4t7w!z(G+KbPeShVmYq3t6A%D*G-KaPdSgVkYp7x!z%C*F-JaNdRgUq4t7w9z$C&F)J@Nc';
 const COOKIE_TIME = 60;
 const BLOCK_TIME = 3;
 
+const END_SLASH = '/';
 const QTY = 8;
 const QTY_LINKS = 3;
 
