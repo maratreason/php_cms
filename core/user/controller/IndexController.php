53ccdddf68a519de9f0813bc5c0072b208b34041
maratreason <maratreason@mail.ru>
2024-08-28 10:43:46 +0300

    chore(user/controller/IndexController)

--- core/user/controller/IndexController.php
+++ core/user/controller/IndexController.php
@@ -15,6 +15,34 @@ class IndexController extends BaseUser
             'order' => ['menu_position']
         ]);
 
-        return compact('sales');
+        $advantages = $this->model->get('advantages', [
+            'where' => ['visible' => 1],
+            'order' => ['menu_position'],
+            'limit' => 6
+        ]);
+
+        $news = $this->model->get('news', [
+            'where' => ['visible' => 1],
+            'order' => ['date'],
+            'order_direction' => ['DESC'],
+            'limit' => 3
+        ]);
+
+        $arrHits = [
+            'hit' => ['name' => 'Хиты продаж', 'icon' => '<svg><use xlink:href="' . PATH . TEMPLATE . 'assets/img/icons.svg#hit"></use></svg>'],
+            'hot' => ['name' => 'Горячие предложения', 'icon' => '<svg><use xlink:href="' . PATH . TEMPLATE . 'assets/img/icons.svg#hot"></use></svg>'],
+            'sale' => ['name' => 'Распродажа', 'icon' => '%'],
+            'new' => ['name' => 'Новинки', 'icon' => 'new'],
+        ];
+        $goods = [];
+
+        foreach ($arrHits as $type => $item) {
+            $goods[$type] = $this->model->getGoods([
+                'where' => [$type => 1, 'visible' => 1],
+                'limit' => 6
+            ]);
+        }
+
+        return compact('sales', 'arrHits', 'goods', 'advantages', 'news');
     }
 }
