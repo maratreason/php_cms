ec84b9c61581cbe81f2cd651a8db3a2522d978a5
maratreason <maratreason@mail.ru>
2024-08-28 10:42:08 +0300

    feat(base/controller/BaseMethods) create method dateFormat()

--- core/base/controller/BaseMethods.php
+++ core/base/controller/BaseMethods.php
@@ -85,4 +85,56 @@ trait BaseMethods
                 PREG_SPLIT_NO_EMPTY
             )[0];
     }
+
+    /**
+     * Форматирование даты для блока новостей
+     *
+     * @param string $date
+     * @return void
+     */
+    protected function dateFormat($date)
+    {
+        if (!$date) {
+            return $date;
+        }
+
+        $daysArr = [
+            'Sunday' => 'Воскресенье',
+            'Monday' => 'Понедельник',
+            'Tuesday' => 'Вторник',
+            'Wednesday' => 'Среда',
+            'Thursday' => 'Четверг',
+            'Friday' => 'Пятница',
+            'Saturday' => 'Суббота'
+        ];
+
+        $monthsArr = [
+            1 => 'Январь',
+            2 => 'Февраль',
+            3 => 'Март',
+            4 => 'Апрель',
+            5 => 'Май',
+            6 => 'Июнь',
+            7 => 'Июль',
+            8 => 'Август',
+            9 => 'Сентябрь',
+            10 => 'Октябрь',
+            11 => 'Ноябрь',
+            12 => 'Декабрь',
+        ];
+
+        $dateArr = [];
+        $dateData = new \DateTime($date);
+
+        $dateArr['year'] = $dateData->format('Y');
+        $dateArr['month'] = $monthsArr[$this->clearNum($dateData->format('m'))];
+        $dateArr['monthFormat'] = preg_match('/т$/u', $dateArr['month']) ? $dateArr['month'] . 'а' : preg_replace('/[ьй]/u', 'я', $dateArr['month']);
+        $dateArr['weekday'] = $daysArr[$dateData->format('l')];
+        $dateArr['day'] = $dateData->format('d');
+        $dateArr['time'] = $dateData->format('H:i:s');
+
+        $dateArr['format'] = mb_strtolower($dateArr['day'] . ' ' . $dateArr['monthFormat'] . ' ' . $dateArr['year']);
+
+        return $dateArr;
+    }
 }
