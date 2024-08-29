61b09be1c94d57e2fa73ace800a0831dacb4feac
maratreason <maratreason@mail.ru>
2024-08-29 18:12:35 +0300

    refactor(user/templates) refactor footer.php

--- templates/default/include/footer.php
+++ templates/default/include/footer.php
@@ -4,7 +4,7 @@
       <div class="footer__wrapper">
         <div class="footer__top">
           <div class="footer__top_logo">
-            <img src="assets/img/Logo.svg" alt="">
+            <img src="<?=$this->img($this->set['img'])?>" alt="">
           </div>
           <div class="footer__top_menu">
             <ul>
