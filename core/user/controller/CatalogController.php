c64e7e31f441221c8cc9a20287f4dda3ccbd5315
maratreason <maratreason@mail.ru>
2024-08-29 11:11:19 +0300

    feat(user/controller/CatalogController) create method inputData()

--- core/user/controller/CatalogController.php
+++ core/user/controller/CatalogController.php
@@ -1,5 +1,8 @@
 <?php
 
+namespace core\user\controller;
+
+use core\base\exceptions\RouteException;
 use core\user\controller\BaseUser;
 
 class CatalogController extends BaseUser
@@ -7,5 +10,40 @@ class CatalogController extends BaseUser
     protected function inputData()
     {
         parent::inputData();
+
+        $order = [
+            'price' => 'цене',
+            'name' => 'названию'
+        ];
+        $data = [];
+
+        if (!empty($this->parameters['alias'])) {
+            $data = $this->model->get('catalog', [
+                'where' => ['alias' => $this->parameters['alias'], 'visible' => 1],
+                'limit' => 1
+            ]);
+
+            if (!$data) {
+                throw new RouteException('Не найдены записи в таблице catalog по ссылке ' . $this->parameters['alias']);
+            }
+    
+            $data = $data[0];
+        }
+
+        $where = ['visible' => 1];
+
+        if ($data) {
+            $where = ['parent_id' => $data['id']];
+        } else {
+            $data['name'] = 'Каталог';
+        }
+
+        $catalogFilters = $catalogPrices = null;
+
+        $goods = $this->model->getGoods([
+            'where' => $where
+        ], $catalogFilters, $catalogPrices);
+
+        return compact('data', 'goods', 'catalogFilters', 'catalogPrices');
     }
 }
