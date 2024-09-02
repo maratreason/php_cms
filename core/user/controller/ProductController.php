98788eb6a144bd7161b4507e44e60ba0bf3248a0
maratreason <maratreason@mail.ru>
2024-09-02 16:17:35 +0300

    feat(user/controller) add ProductController

---
+++ core/user/controller/ProductController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace core\user\controller;
+
+use core\base\exceptions\RouteException;
+
+class ProductController extends BaseUser
+{
+    protected function inputData()
+    {
+        parent::inputData();
+
+        if (empty($this->parameters['alias'])) {
+            throw new RouteException('Отсутствует ссылка на товар');
+        }
+
+        $data = $this->model->getGoods([
+            'where' => ['alias' => $this->parameters['alias'], 'visible' => 1],
+        ]);
+
+        if (!$data) {
+            throw new RouteException('Отсутствует товар по ссылке ' . $this->parameters['alias']);
+        }
+
+        $data = array_shift($data);
+
+        return compact('data');
+    }
+}
