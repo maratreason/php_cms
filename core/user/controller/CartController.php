f2e8a61c65dd1a85ab32e35a14d2ef29b8f2bbec
maratreason <maratreason@mail.ru>
2024-09-06 17:18:27 +0300

    feat(user/controller/CartController) create add product to basket

--- core/user/controller/CartController.php
+++ core/user/controller/CartController.php
@@ -4,8 +4,24 @@ namespace core\user\controller;
 
 class CartController extends BaseUser
 {
+    protected $delivery;
+    protected $payments;
+
     protected function inputData()
     {
         parent::inputData();
+
+        $this->delivery = $this->model->get('delivery');
+        $this->payments = $this->model->get('payments');
+
+        if (!empty($this->parameters['alias']) && $this->parameters['alias'] === 'remove') {
+            if (!empty($this->parameters['id'])) {
+                $this->deleteCartData($this->parameters['id']);
+            } else {
+                $this->clearCart();
+            }
+
+            $this->redirect($this->alias('cart'));
+        }
     }
 }
