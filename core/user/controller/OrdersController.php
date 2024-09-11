b53b2326d8e4c15e895eb064be8ac28482b12340
maratreason <maratreason@mail.ru>
2024-09-11 11:01:57 +0300

    feat(user/controllerOrdersController) add method setOrdersGoods()

--- core/user/controller/OrdersController.php
+++ core/user/controller/OrdersController.php
@@ -2,6 +2,7 @@
 
 namespace core\user\controller;
 
+use core\base\model\UserModel;
 use core\user\helpers\ValidationHelper;
 
 class OrdersController extends BaseUser
@@ -76,6 +77,110 @@ class OrdersController extends BaseUser
             }
         }
 
+        if (empty($visitor['email']) && empty($visitor['phone'])) {
+            $this->sendError('Отсутствуют данные пользователя для оформления заказа');
+        }
+
+        $visitorsWhere = $visitorsCondition = [];
+
+        if (!empty($visitor['email']) && !empty($visitor['phone'])) {
+            $visitorsWhere = [
+                'email' => $visitor['email'],
+                'phone' => $visitor['phone'],
+            ];
+
+            $visitorsCondition = ['OR'];
+        } else {
+            $visitorsKey = !empty($visitor['email']) ? 'email' : 'phone';
+            $visitorsWhere[$visitorsKey] = $visitor[$key];
+        }
+
+        $resVisitor = $this->model->get('visitors', [
+            'where' => $visitorsWhere,
+            'condition' => $visitorsCondition,
+            'limit' => 1
+        ]);
+
+        if ($resVisitor) {
+            $resVisitor = $resVisitor[0];
+            $order['visitors_id'] = $resVisitor[0]['id'];
+        } else {
+            // Регистрируем пользователя
+            $order['visitors_id'] = $this->model->add('visitors', [
+                'fields' => $visitor,
+                'return_id' => true
+            ]);
+        }
+
+        $order['total_sum'] = $this->cart['total_sum'];
+        $order['total_qty'] = $this->cart['total_qty'];
+        $order['total_old_sum'] = $this->cart['total_old_sum'] ?? 0;
+
+        $baseStatus = $this->model->get('orders_statuses', [
+            'fields' => ['id'],
+            'order' => ['menu_position'],
+            'limit' => 1
+        ]);
+
+        $baseStatus && $order['orders_statuses_id'] = $baseStatus[0]['id'];
+
+        $order['id'] = $this->model->add('orders', [
+            'fields' => $order,
+            'return_id' => true
+        ]);
+
+        if (!$order['id']) {
+            $this->sendError('Ошибка сохранения заказа. Свяжитесь с администрацией сайта по телефону - ' . $this->set['phone']);
+        }
+
+        if (!$resVisitor) {
+            UserModel::instance()->checkUser($order['visitors_id']);
+        }
+
+        if (!$this->setOrdersGoods($order)) {
+            $this->sendError('Ошибка сохранения товаров заказа. Обратитесь к администрации сайта');
+        }
+
+        $this->setOrdersGoods($order);
+
+        $this->sendSuccess('Спасибо за заказ. В ближайшее время наши менеджеры свяжутся с Вами для уточнения деталей');
+
+        $this->sendOrderEmail(['order' => $order, 'visitor' => $visitor]);
+        $this->clearCart();
+        $this->redirect();
+    }
+
+    protected function setOrdersGoods(array $order): bool
+    {
+        if (in_array('orders_goods', $this->model->showTables())) {
+            $ordersGoods = [];
+
+            foreach ($this->cart['goods'] as $key => $item) {
+                $ordersGoods[$key]['orders_id'] = $order['id'];
+
+                foreach ($item as $field => $value) {
+                    if (!empty($this->model->showColumns('orders_goods')[$field])) {
+                        if ($this->model->showColumns('orders_goods')['id_row'] === $field) {
+                            if (!empty($this->model->showColumns('orders_goods')['goods_id'])) {
+                                $ordersGoods[$key]['goods_id'] = $value;
+                            }
+                        } else {
+                            $ordersGoods[$key][$field] = $value;
+                        }
+                    }
+                }
+            }
+
+            return $this->model->add('orders_goods', [
+                'fields' => $ordersGoods
+            ]);
+        }
+
+        return false;
+    }
+
+    protected function sendOrderEmail(array $orderData)
+    {
 
     }
 }
